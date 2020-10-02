d5402095d75571a59e9f43ac72532270782e3241
andryudianto <andriyudianto60@gmail.com>
2020-10-02 13:19:21 +0700

    udah dpt data dari api

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const confirmPassword = require ('../middlewares/confirm_pass')
 const UserController = require ('../controllers/users')
 const App = require('../controllers/app')
 const authentication = require('../middlewares/authentication')
+const ThirdParty = require('../controllers/thirdParty')
 
 router.post('/apps', App.create)
 router.delete('/apps/:id', authentication,  App.delete)
@@ -11,7 +12,8 @@ router.post('/register', confirmPassword, UserController.register)
 router.post('/login', UserController.login)
 router.put('/edit/:id', authentication, App.editAll)
 router.patch('/edit/:id', authentication, App.changeStatus)
+router.get('/location', ThirdParty.location)
+router.get('/weather', ThirdParty.weather)
+router.get('/random', ThirdParty.random)
 
-
-
-module.exports =router
+module.exports = router
